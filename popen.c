0cbd16ae6285cf49420600619d9903cd052148b0
mikeZhang1 <1946658040@qq.com>
2016-11-23 15:57:52 -0500

    Update popen.c

--- popen.c
+++ popen.c
@@ -1,50 +1,32 @@
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-int
-main ()
+int main(void)
 {
-  FILE *ps_pipe;
-  FILE *grep_pipe;
+        FILE *pipein_fp, *pipeout_fp;
+        char readbuf[80];
 
-  int bytes_read;
-  int nbytes = 100;
-  char *my_string;
+        /* Create one way pipe line with call to popen() */
+        if (( pipein_fp = popen("ls", "r")) == NULL)
+        {
+                perror("popen");
+                exit(1);
+        }
 
-  /* Open our two pipes */
-  ps_pipe = popen ("ps -A", "r");
-  grep_pipe = popen ("grep init", "w");
+        /* Create one way pipe line with call to popen() */
+        if (( pipeout_fp = popen("sort", "w")) == NULL)
+        {
+                perror("popen");
+                exit(1);
+        }
 
-  /* Check that pipes are non-null, therefore open */
-  if ((!ps_pipe) || (!grep_pipe))
-    {
-      fprintf (stderr,
-               "One or both pipes failed.\n");
-      return EXIT_FAILURE;
-    }
+        /* Processing loop */
+        while(fgets(readbuf, 80, pipein_fp))
+                fputs(readbuf, pipeout_fp);
 
-  /* Read from ps_pipe until two newlines */
-  my_string = (char *) malloc (nbytes + 1);
-  bytes_read = getdelim (&my_string, &nbytes, "\n\n", ps_pipe);
+        /* Close the pipes */
+        pclose(pipein_fp);
+        pclose(pipeout_fp);
 
-  /* Close ps_pipe, checking for errors */
-  if (pclose (ps_pipe) != 0)
-    {
-      fprintf (stderr,
-               "Could not run 'ps', or other error.\n");
-    }
-
-  /* Send output of 'ps -A' to 'grep init', with two newlines */
-  fprintf (grep_pipe, "%s\n\n", my_string);
-
-  /* Close grep_pipe, cehcking for errors */
-  if (pclose (grep_pipe) != 0)
-    {
-      fprintf (stderr,
-               "Could not run 'grep', or other error.\n");
-    }
-
-  /* Exit! */
-  return 0;
+        return(0);
 }
