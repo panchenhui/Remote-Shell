df7505c274f18ab3cf253b2ddc9d72c0d2344b3c
panchenhui <pan.558@osu.edu>
2016-11-29 22:00:53 -0500

    Deleted useless comments

--- server.c
+++ server.c
@@ -16,95 +16,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>	// file status 
 
-/* Function: check the access status for the file
-	print out the status message for the file. 
-*/
-
-/*
-char* access_status(char* filename) {
-	char* message[2000];
-
-	// check whether the file could be written. 
-	if(access(filename, F_OK) == 0){
-		strcpy(message, "The file exists.");
-	}else{
-		strcpy(message, "The file does not exist.");
-	}
-
-	// check whether the file could be red.
-	if(access(filename, R_OK) == 0){
-		strcpy(message, "The file could be read.");
-	}else{
-		strcpy(message, "The file could not be read.");
-	}
-
-	// check whether the file could be written. 
-	if(access(filename, W_OK) == 0){
-		strcpy(message, "The file could be written.");
-	}else{
-		strcpy(message, "The file could not be written.");
-	}
-	return message;
-}
-*/
-
-/*  
-	Function: change file permission
-	return the prompt message back to screen
-*/
-/*
-char* change_file_mode(char* filename, char* mode){
-	char* message[2000];
-	// set file to read permission for owner
-	if(strcmp(mode, "user_read") == 0){
-		chmod(filename, S_IRUSR);
-		strcpy(message, "The file has been changed to read_only mode for owner"); 
-
-	// set file to write permission for owner
-	}else if(strcmp(mode, "user_write") == 0){
-		chmod(filename, S_IWUSR);
-		strcpy(message, "The file has been changed to write_only mode for owner"); 
-
-	// set file to read permission for others
-	}else if(strcmp(mode, "other_read"){
-		chmod(filename, S_IROTH);
-		strcpy(message, "The file has been changed to read_only mode for others"); 
-
-	// set file to write permission for others
-	}else if(strcmp(mode, "other_write"){
-		chmod(filename, S_IWOTH);
-		strcpy(message, "The file has been changed to write_only mode for others");
-	}else{
-		strcpy(message, "The mode entered does not exist !"); 
-	}
-	return message;
-}*/
-
-/* 
-	Function: call linux command
-	pass the linux commnad and do system call
-*/
-void call_command(char* command){
-	if(strcmp(command, "ls -l") == 0){
-		printf("List all the file in current directory.\n");
-	}
-	system(command);
-}
-
-/* 
-	Function: remove the file
-*/
-/*
-char* remove_file(char* filename){
-	char* message[200];
-	if( remove(filename) == 0 ){ 
-		strcpy(message, "Removed");
-	}else{
-		strcpy(message, "Removed failed.");
-	}
-	return message
-}*/
-
 int transfer_file(char* file_name, int csock){
      FILE *fp = fopen(file_name, "rb");
     if (fp == NULL)
@@ -227,37 +138,6 @@ int main(int argc , char *argv[])
         printf("Invalid command\n");
     }
 
-/*
-    // Send file to the client
-    FILE *fp = fopen("server_log.txt", "rb");
-    if (fp == NULL)
-    {
-        printf("File open failed\n");
-        return 1;
-    }
-
-
-    while(1){
-        unsigned char buffer[BUFFER] = {0};
-        int nread = fread(buffer,1, BUFFER, fp);
-        printf("Bytes read from the txt file: %d\n",nread);
-
-        if (nread > 0)
-        {
-            printf("Sending...\n");
-            write(csock,buffer,nread);
-        }
-
-         if (nread < BUFFER)
-        {
-            if (feof(fp))
-                printf("End of file\n");
-            if (ferror(fp))
-                printf("Error reading\n");
-            break;
-        }
-    }
-*/
 
     printf("The file has been sent to the client\n");
 
