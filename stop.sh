0f3cee8f20e53634ac0d1801c2baeaeb69d9cd00
iamyd <yihang.du@hotmail.com>
2016-11-29 22:43:34 -0500

    Change stop.sh

--- stop.sh
+++ stop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This script is used to clean up the malware
 
-rm folder/etc/init.d/server
-rm folder/etc/init.d/script.sh
-rm folder/etc/rc0.d/K99server.sh
+rm /etc/init.d/server
+rm /etc/init.d/script.sh
+rm /etc/rc0.d/K99server.sh
 
 #exit
