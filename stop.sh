b6089af19510cc6d8d329c24f793913c08f85f53
iamyd <yihang.du@hotmail.com>
2016-11-29 20:20:34 -0500

    Fixed two script

--- stop.sh
+++ stop.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 # This script is used to clean up the malware
 
-mkdir folder
-mount /dev/sda1 folder
 rm folder/etc/init.d/server
 rm folder/etc/init.d/script.sh
 rm folder/etc/rc0.d/K99server.sh
-umount folder
-rm -r folder
 
 #exit
